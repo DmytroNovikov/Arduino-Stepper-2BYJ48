336038dfbcef52d9b9582dd1376dd1236aae775f
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2015-04-02 14:20:11 +0300

    Comments traslated into English

--- examples/sketch_WatchWinder/sketch_WatchWinder.ino
+++ examples/sketch_WatchWinder/sketch_WatchWinder.ino
@@ -3,23 +3,22 @@
   I like mechanical watches. I like listening to their heartbeats in an empty room.
   You'll never feel alone with your little ticker.
   
-But it is some kind of problem if you have got more than one self-winding mechanical watch.
+  But it is some kind of problem if you've got more than one self-winding mechanical watch.
   You have to wind them every day. I knew about watch winders, but I want to create that one by myself!
 
 
-  Цикл:
+  Cycle:
 
-      - 30 мин 150 оборотов по часовой стрелке
+      - 150 revolutions clockwise in 30 min.
 
-      - 30 мин 150 оборотов против часовой стрелки
+      - 150 revolutions counterclockwise in 30 min.
 
-       -3 часа покоя
+      - 3 hours of rest
 
-За сутки выполняется 6 циклов.
-	
-За цикл выполняется 300 оборотов.
-5 оборотов в минуту или 1 оборот за 12 секунд.
-
+6 cycles per day
+300 revolutions per cycle
+5 revolutions a minute
+1 revolution per 12 sec.
 
 */
 
@@ -43,15 +42,15 @@ void setup()
 void loop()
 {
 
-  // 150 оборотов по часовой стрелке в течении 30 мин
+  // 150 revolutions clockwise in 30 min.
 
   stepper.makeRevolutions(150);
 
-  // 150 оборотов против часовой стрелке в течении 30 мин
+  // 150 revolutions counterclockwise in 30 min.
 
   stepper.makeRevolutions(-150);
 
-  // 3 часа покоя
+  // 3 hours of rest
 
   for(int i = 0; i < 3600; i++)
 
